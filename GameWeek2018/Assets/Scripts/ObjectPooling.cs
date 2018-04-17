71343e9362a4378b13f237753779fced360f9297
Unknown <philippe.campion.gingras@hotmail.com>
2018-04-17 13:35:39 -0400

    Added WorldCreator

---
+++ GameWeek2018/Assets/Scripts/ObjectPooling.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooling : MonoBehaviour
+{
+    [SerializeField] private int            instantiationCount;
+    [SerializeField] private List<Chunk>    chunkPrefabs;
+
+    private Dictionary<int, Queue<Chunk>> chunkTypePooling;
+
+	// Use this for initialization
+	void Start ()
+    {
+        chunkTypePooling = new Dictionary<int, Queue<Chunk>>();
+        FillPools();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void FillPools()
+    {
+        int index = 0;
+        Chunk currentChunk;
+        foreach(Chunk prefab in chunkPrefabs)
+        {
+            chunkTypePooling[index] = new Queue<Chunk>();
+            for (int i = 0; i < instantiationCount; ++i)
+            {
+                currentChunk = Instantiate(prefab);
+                currentChunk.gameObject.SetActive(false);
+                currentChunk.transform.SetParent(gameObject.transform);
+                chunkTypePooling[index].Enqueue(currentChunk);
+            }
+            ++index;
+        }
+    }
+}
---
+++ GameWeek2018/Assets/Scripts/WorldCreator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class WorldCreator : MonoBehaviour
+{
+    [SerializeField] private int chunkDistance;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
