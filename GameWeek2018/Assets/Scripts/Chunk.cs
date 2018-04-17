6b0d01dd28ad1a9ef6c3f491e27bc401a23b3e77
Unknown <philippe.campion.gingras@hotmail.com>
2018-04-17 15:03:13 -0400

    Added ChunkGenerator

--- GameWeek2018/Assets/Scripts/Chunk.cs
+++ GameWeek2018/Assets/Scripts/Chunk.cs
@@ -1,9 +1,19 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Chunk : MonoBehaviour
 {
+    [Header("Entry and Exit Points")]
     [SerializeField] private Transform entryPoint;
     [SerializeField] private Transform exitPoint;
+
+    [Header("Obstacles")]
+    [SerializeField] private bool           generateObstacles;
+    [SerializeField] private int            minObstaclesCount;
+    [SerializeField] private int            maxObstaclesCount;
+    [SerializeField] private List<string>   obstaclesID;
+    [Range(0, 100)]
+    [SerializeField] private List<float>    obstacleSpawnChance;
 
 	// Use this for initialization
 	void Start ()
@@ -16,4 +26,14 @@ public class Chunk : MonoBehaviour
     {
 		
 	}
+
+    private void SpawnObstacles()
+    {
+
+    }
+
+    public void InitAndPlace(Transform lastChunkExitPoint)
+    {
+        
+    }
 }
