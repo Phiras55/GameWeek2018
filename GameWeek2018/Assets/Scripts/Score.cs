161a91e86ff394eabbe06d38e2490e3f881511db
Vincent Dupont <vincent.dupont94@hotmail.fr>
2018-04-20 08:39:37 +0200

    Added music

---
+++ GameWeek2018/Assets/Scripts/Score.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    private float score = 0.0f;
+
+    public Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
